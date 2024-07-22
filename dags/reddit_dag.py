7271351722c92296c19da8a23c538da244db3706
Promchok <promchok.i@gmail.com>
2024-07-22 15:48:49 +0700

    Create tasks to upload csv to s3

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -1,7 +1,15 @@
-from airflow import DAG
+import os
+import sys
 from datetime import datetime
+
+from airflow import DAG
 from airflow.operators.python import PythonOperator
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from pipelines.reddit_pipeline import reddit_pipeline
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
+
 
 default_args = {
     'owner': 'Promchok',
@@ -31,5 +39,12 @@ extract = PythonOperator(
     dag=dag
 )
 
-
 # upload to s3
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable=upload_s3_pipeline,
+    dag=dag
+)
+
+
+extract >> upload_s3
